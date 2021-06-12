c211fe068b47f1fc59771bc30e6a596ea3e9fa14
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-13 09:03:42 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapTowNumbersUsingTemporaryVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("Please enter first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("After Swapping :");
+            Console.WriteLine("First Number : " + number1);
+            Console.WriteLine("Second Number : " + number2);
+            Console.ReadLine();
+
+        }
+    }
+}
